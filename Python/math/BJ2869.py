55fa929bb64275cda82ab3500952013ff9d45261
JiHooney <hoeeyz1@gmail.com>
2021-07-24 19:05:27 +0900

    studying

---
+++ Python/math/BJ2869.py
@@ -0,0 +1,4 @@
+a, b, v = map(int, input().split())
+res = (v-b)//(a-b)
+if (v-b)%(a-b) != 0: res += 1
+print( res )
---
+++ Python/string/BJ1259.py
@@ -0,0 +1,9 @@
+while True:
+    n = input()
+    if n=="0": break
+    ans = "yes"
+    for i in range( len(n)//2 ):
+        if n[i] != n[len(n)-i-1]: 
+            ans="no"
+            break
+    print( ans )
