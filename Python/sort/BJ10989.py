75e6b9792293a7e404a4f96a96ad7aada7fc0b9c
JiHooney <hoeeyz1@gmail.com>
2021-07-27 10:33:42 +0900

    studying

---
+++ Python/sort/BJ10989.py
@@ -0,0 +1,16 @@
+import sys
+readline = sys.stdin.readline
+
+def main():
+    n = int( readline() )
+    arr = [ 0 for _ in range( 10001 ) ]
+    
+    for _ in range( n ):
+        arr[ int(readline()) ] += 1
+        
+    for i in range( len(arr) ):
+        for j in range( arr[i] ):
+            print( i )
+            
+if __name__ == '__main__':
+    main()
