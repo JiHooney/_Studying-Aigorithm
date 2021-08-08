720f5ff338619bc729b7076797c6ff19f9e3e6d5
JiHooney <hoeeyz1@gmail.com>
2021-08-08 22:49:44 +0900

    studying

--- src/main/java/bfs_dfs/BJ2630.java
+++ src/main/java/bfs_dfs/BJ2630.java
@@ -3,19 +3,34 @@ package bfs_dfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BJ2630 {
+	
+	private static void cutPaper( int white, int blue, int start, int end, int[][] arr ) {
+		
+		int tmp = 0;
+		for( int i=start; i<end; i++ )
+			for( int j=start; j<end; j++ )
+				tmp++;
+		
+	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt( br.readLine() );
-		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st;
+		int[][] arr = new int[n][n];
 		for( int i=0; i<n; i++ ) {
-			for( int j=n-i; j>0; j-- )
-				sb.append( "*" );
-			
-			sb.append( "\n" );
+			st = new StringTokenizer( br.readLine() );
+			for( int j=0; j<n; j++ ) arr[i][j] = Integer.parseInt( st.nextToken() );
 		}
-		System.out.println( sb );
+		
+		int white = 0;
+		int blue = 0;
+		cutPaper( white, blue, 0, arr.length, arr );
+		
+		
 	}
 }
